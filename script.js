70675ee41d5592579b28530505ba4ba45c8b810e
Charalamboss <ch.pouroutides@gmail.com>
2017-06-17 12:28:18 +0300

    grids created but need to fit the main box

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var test=10;
+var test=1000;
 
 
 
@@ -22,19 +22,22 @@ $(document).ready(function(){
 	//Create empty DIVS with with the style properties of the cell and then add them to the box
 	//more infor at https://stackoverflow.com/questions/10619445/the-preferred-way-of-creating-a-new-element-with-jquery
 		$( "<div></div>", {
-  						"class": "grid"
+  						"class": "grid" , id:"grid"+i // use it for each Div to be unique when i hover, better to use "this" in the $function
   							})
 						.appendTo(".container");
+		
 	}
+
+
 	// when enter to DIVs change the color, it doesnt work
 	$('.grid').mouseenter(function(){
-		$('.grid').css("background-color","green");
+		$(this).css("background-color","green");// use "this" to change only the selected div and not all 
 		
 	});
 
-	$(".grid").mouseleave(function(){
+	/*$(".grid").mouseleave(function(){
         $(".grid").css("background-color","red");
-    })
+    })*/
 });
 
 
